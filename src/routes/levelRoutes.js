eb674cb7634e652bc9de9288c895a8e108fbda4f
herff228 <vladeli40@gmail.com>
2026-03-04 00:20:00 +0700

    Добавлен эндпоинт /api/levels/all для получения всех заданий

--- src/routes/levelRoutes.js
+++ src/routes/levelRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const { getTaskForLevel, completeLevelTask, restartGame } = require('../controllers/levelController');
+const { 
+  getTaskForLevel, 
+  completeLevelTask, 
+  restartGame,
+  getAllTasksHistory,
+  getAllTasks 
+} = require('../controllers/levelController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/:level/task', getTaskForLevel);
-router.post('/:level/complete', completeLevelTask);
-router.post('/restart', restartGame);
+router.get('/all', getAllTasks);              // ПОЛУЧИТЬ ВСЕ ЗАДАНИЯ СРАЗУ
+router.get('/:level/task', getTaskForLevel);  // ПОЛУЧИТЬ ОДНО ЗАДАНИЕ
+router.post('/:level/complete', completeLevelTask); // ВЫПОЛНИТЬ ЗАДАНИЕ
+router.post('/restart', restartGame);          // ПЕРЕЗАПУСТИТЬ ИГРУ
+router.get('/history/all', getAllTasksHistory); // ИСТОРИЯ ВСЕХ ЗАДАНИЙ
 
 module.exports = router;
